a5277be404d0f83e4d0ae4458eda71d658ab7286
nishanthrajan <47620995+nishanthrajan@users.noreply.github.com>
2019-08-15 07:28:13 +0000

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,k,i;
+printf("enter the values of n and k:");
+scanf("%d%d",&n,&k);
+for(i=n;i<=k;i++)
+{
+	if(i%2!=0)
+	{
+		printf("\n%d\n",i);
+	}
+}
+}
